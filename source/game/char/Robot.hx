b0125da4ce26e447b2846f6ddcd61bca0ede969a
KinoAR <kinoarose@gmail.com>
2021-11-07 17:41:31 -0500

    Setup the remote bomb skill for the robot character

--- source/game/char/Robot.hx
+++ source/game/char/Robot.hx
@@ -116,13 +116,47 @@ class Robot extends BaseChar {
 
   public function updateAbility(elapsed:Float) {
     if (controller == PlayerOne && FlxG.keys.pressed.N && cdTimer <= 0) {
-      cdTimer = ABILITY_CD;
+      // TODO make as a single function for convenience
+      handleBomb();
     }
 
-    if (controller == PlayerTwo && FlxG.keys.pressed.W && cdTimer <= 0) {
-      cdTimer = ABILITY_CD;
+    if (controller == PlayerTwo && FlxG.keys.pressed.W) {
+      handleBomb();
     }
+
     // Cooldown constantly lowered to 0
     cdTimer = (cdTimer - elapsed).clampf(0, ABILITY_CD);
+  }
+
+  public function handleBomb() {
+    if (hasRemote()) {
+      var rBomb:RemoteBomb = cast getRemote();
+      if (rBomb != null) {
+        rBomb.detonate();
+      }
+    }
+    if (cdTimer <= 0) {
+      cdTimer = ABILITY_CD;
+      placeRemoteBomb(this.x - this.offset.x, this.y - this.offset.y);
+    }
+  }
+
+  public function placeRemoteBomb(x:Float, y:Float) {
+    var bomb = new RemoteBomb(x, y, explosionGroup);
+    bomb.presentBombTimer = 30;
+    bombGroup.add(bomb);
+    bombDropSound.play(true);
+  }
+
+  public function hasRemote() {
+    return bombGroup.members.exists((bomb:Bomb) -> {
+      return Std.isOfType(bomb, RemoteBomb) && bomb.alive;
+    });
+  }
+
+  public function getRemote() {
+    return bombGroup.members.filter((bomb:Bomb) -> {
+      return Std.isOfType(bomb, RemoteBomb) && bomb.alive;
+    })[0];
   }
 }
