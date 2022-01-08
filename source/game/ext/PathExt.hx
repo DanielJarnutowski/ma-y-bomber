c8b5548305cfe22f171e9ea660174d7477b1405b
KinoAR <kinoarose@gmail.com>
2022-01-08 16:38:55 -0500

    Add update the commands for chokidar

--- source/game/ext/PathExt.hx
+++ source/game/ext/PathExt.hx
@@ -2,7 +2,9 @@ package game.ext;
 
 import flixel.util.FlxPath;
 
-inline function activePathNode(path:FlxPath):FlxPoint {
-	var currentPoint = path.nodes[path.nodeIndex];
-	return currentPoint;
+class PathExt {
+  public static inline function activePathNode(path:FlxPath):FlxPoint {
+    var currentPoint = path.nodes[path.nodeIndex];
+    return currentPoint;
+  }
 }
--- source/game/ext/SpriteExt.hx
+++ source/game/ext/SpriteExt.hx
@@ -3,33 +3,39 @@ package game.ext;
 import flixel.FlxCamera;
 import flixel.util.FlxAxes;
 
-inline function screenCenterHorz(sprite:FlxSprite) {
-	sprite.screenCenter(FlxAxes.X);
-}
+class SpriteExt {
+  public static inline function screenCenterHorz(sprite:FlxSprite) {
+    sprite.screenCenter(FlxAxes.X);
+  }
 
-inline function cameraCenter(sprite:FlxSprite, ?camera:FlxCamera) {
-	camera = camera == null ? sprite.camera : camera;
-	sprite.cameraCenterHorz(camera);
-	sprite.cameraCenterVert(camera);
-}
+  public static inline function cameraCenter(sprite:FlxSprite,
+      ?camera:FlxCamera) {
+    camera = camera == null ? sprite.camera : camera;
+    sprite.cameraCenterHorz(camera);
+    sprite.cameraCenterVert(camera);
+  }
 
-inline function cameraCenterHorz(sprite:FlxSprite, ?camera:FlxCamera) {
-	camera = camera == null ? sprite.camera : camera;
-	sprite.x = ((camera.x + camera.width) / 2) - (sprite.width / 2);
-}
+  public static inline function cameraCenterHorz(sprite:FlxSprite,
+      ?camera:FlxCamera) {
+    camera = camera == null ? sprite.camera : camera;
+    sprite.x = ((camera.x + camera.width) / 2) - (sprite.width / 2);
+  }
 
-inline function screenCenterVert(sprite:FlxSprite) {
-	sprite.screenCenter(FlxAxes.Y);
-}
+  public static inline function screenCenterVert(sprite:FlxSprite) {
+    sprite.screenCenter(FlxAxes.Y);
+  }
 
-inline function cameraCenterVert(sprite:FlxSprite, ?camera:FlxCamera) {
-	camera = camera == null ? sprite.camera : camera;
-	sprite.y = ((camera.y + camera.height) / 2) - (sprite.height / 2);
-}
+  public static inline function cameraCenterVert(sprite:FlxSprite,
+      ?camera:FlxCamera) {
+    camera = camera == null ? sprite.camera : camera;
+    sprite.y = ((camera.y + camera.height) / 2) - (sprite.height / 2);
+  }
 
-inline function drawBorder(sprite:FlxSprite, size:Float, color:FlxColor) {
-	sprite.drawRect(0, 0, sprite.width, sprite.height, KColor.TRANSPARENT, {
-		thickness: size,
-		color: color,
-	});
+  public static inline function drawBorder(sprite:FlxSprite, size:Float,
+      color:FlxColor) {
+    sprite.drawRect(0, 0, sprite.width, sprite.height, KColor.TRANSPARENT, {
+      thickness: size,
+      color: color,
+    });
+  }
 }
